70fc550189886f68de5773b3e3315664f2856286
shawn <axe2781@gmail.com>
2018-01-30 11:43:18 -0500

    bbs

--- AutoSystems/Infrastructure/BlogRepoFs.cs
+++ AutoSystems/Infrastructure/BlogRepoFs.cs
@@ -8,29 +8,87 @@ namespace Infrastructure
 {
     public class BlogRepoFs : IBlogPostsRepo
     {
+        private static List<BlogRepoFs> _BlogRepoFs;
+        private static int _nextId = 1;
+        
+        private const string PATH = "data";
+        private const string FILENAME = ".json";
+
+        private readonly string _fileFullPath = Path.Combine(PATH, FILENAME);
+
+        public BlogRepoFs()
+        {
+            if (_BlogRepoFs == null)
+            {
+                _BlogRepoFs = LoadFile();
+
+                if (_BlogRepoFs.Count > 0)
+                {
+                    _nextId = _BlogRepoFs.Max(t => t.Id) + 1;
+                }
+            }
+        }
+
         public void Add(BlogPost newpost)
         {
-            throw new NotImplementedException();
+            newBlogPost.permalink = _nextId++;
+            _BlogRepoFs.Add(newBlogPost);
+
+            SaveFile();
         }
 
         public void Delete(BlogPost newpost)
         {
-            throw new NotImplementedException();
-        }
+            var origTelegram = GetById(newpost.permalink);
 
+            _BlogRepoFs.Remove(newBlogPost);
+
+            SaveFile();
+        }
         public void Edit(BlogPost newpost)
         {
-            throw new NotImplementedException();
-        }
+            var newBlogPost = GetById(newpost.permalink);
 
+            newBlogPost.Content = newpost.Content;
+            newBlogPost.Receiver = newpost.Receiver;
+            newBlogPost.Sender = newpost.Sender;
+            newBlogPost.Title = newpost.Title;
+
+            SaveFile();
+        }
         public BlogPost GetPostByPermalink(string permalink)
         {
-            throw new NotImplementedException();
+            return _BlogRepoFs.Find(t => t.permalink == permalink);
         }
 
         public List<BlogPost> ListAll()
         {
-            throw new NotImplementedException();
+            return _BlogRepoFs;
         }
+
+        //public void Add(BlogPost newpost)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Delete(BlogPost newpost)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Edit(BlogPost newpost)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public BlogPost GetPostByPermalink(string permalink)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public List<BlogPost> ListAll()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
---
+++ AutoSystems/Infrastructure/BlogRepoMemory.cs
@@ -0,0 +1,81 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class BlogRepoMemory : IBlogPostsRepo
+    {
+        private static List<BlogPost> _BlogPost;
+        private static int _nextId = 1;
+        private int id;
+
+        public BlogRepoMemory()
+        {
+            if (_BlogPost == null)
+            {
+                _BlogPost = new List<BlogPost>();
+            }
+        }
+
+        public void Add(BlogPost newBlogPost)
+        {
+            newBlogPost.Id = _nextId++;
+            _BlogPost.Add(newBlogPost);
+        }
+
+        public void Delete(BlogPost newpostDelete)
+        {
+            var origBlogPost = GetById(newpostDelete.Id);
+
+            _BlogPost.Remove(origBlogPost);
+        }
+
+        public void Edit(BlogPost newpost)
+        {
+            var origTelegram = GetById(newpost.Id);
+
+            origBlogPost.Content = newpost.Content;
+            origBlogPost.Receiver = newpost.Receiver;
+            origBlogPost.Sender = newpost.Sender;
+            origBlogPost.Title = newpost.Title;
+        }
+
+        public BlogPost GetPostByPermalink(string permalink)
+        {
+            return _BlogPost.Find(t => t.Id == id);
+        }
+
+        public List<BlogPost> ListAll()
+        {
+            return _BlogPost;
+        }
+
+        //public void Add(BlogPost newpost)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Delete(BlogPost newpost)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Edit(BlogPost newpost)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public BlogPost GetPostByPermalink(string permalink)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public List<BlogPost> ListAll()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
